5139fa463b05956c7fd9d9c36f6b3849219aadb8
sp13011996 <githubprojects0508@gmail.com>
2022-06-27 08:06:58 +0530

    login and register add

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const booksRoute = require('./Server/Routes/bookroute')
 const path = require('path')
+const User = require('./Server/Models/user.model')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const cors = require('cors')
+
 
 const winston = require('winston')
 
@@ -11,6 +16,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 //MIDDLEWARE
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
@@ -23,6 +29,50 @@ app.use(express.static(path.join(__dirname, "/frontend/build")))
 
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+})
+
+app.post('/api/register', async (req, res) => {
+    console.log(req.body)
+    try {
+        const newPassword = await bcrypt.hash(req.body.password, 10)
+        await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: newPassword,
+        })
+        res.json({ status: 'ok' })
+    } catch (err) {
+        res.json({ status: 'error', error: 'Duplicate email' })
+    }
+})
+
+app.post('/api/login', async (req, res) => {
+    const user = await User.findOne({
+        email: req.body.email,
+    })
+
+    if (!user) {
+        return { status: 'error', error: 'Invalid login' }
+    }
+
+    const isPasswordValid = await bcrypt.compare(
+        req.body.password,
+        user.password
+    )
+
+    if (isPasswordValid) {
+        const token = jwt.sign(
+            {
+                name: user.name,
+                email: user.email,
+            },
+            'secret123'
+        )
+
+        return res.json({ status: 'ok', user: token })
+    } else {
+        return res.json({ status: 'error', user: false })
+    }
 })
 
 //CREATE A LOGGER
